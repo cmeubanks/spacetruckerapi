da897bb4760a017b8f5d18f3574ccf471a3a4467
Caroline Eubanks <cmeubanks@gmail.com>
2021-06-03 16:37:54 -0600

    removed ssl

--- knexfile.js
+++ knexfile.js
@@ -14,12 +14,12 @@ module.exports = {
   },
 
   production: {
-  client: 'pg',
-  connection: process.env.DATABASE_URL + `?ssl=true`,
-  migrations: {
-    directory: './db/migrations'
-  },
-  useNullAsDefault: true
-}
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
 
 };
